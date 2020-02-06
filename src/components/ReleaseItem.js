1239fa9b9f6a79bcb6c34f20f2db89bbe52cfcd3
Brittany Houtz <brittany.houtz@gmail.com>
2020-02-06 15:31:07 -0800

    ArtistDetail component added

---
+++ src/components/ReleaseItem.js
@@ -0,0 +1,31 @@
+// import React from 'react';
+// import PropTypes from 'prop-types';
+// // import {  } from hooks
+
+// const ArtistDetail = ({ match }) => {
+//   const artist = useArtist(match.params.id);
+
+//   const releaseLinks = artist.release.map(url => (
+//     <a key={url} href={url}>Release {url.split('/').splice(-1)[0]}</a>
+//   ));
+
+//   return (
+//     <section>
+//       <h1>The Beatles</h1>
+//       <ul>
+//         <li>
+//           <p>Please Please Me</p>
+//           <img src="https://ia801905.us.archive.org/20/items/mbid-ee397a6d-c56f-45ea-9955-d203d2fea708/mbid-ee397a6d-c56f-45ea-9955-d203d2fea708-15141015861.jpg" />
+//         </li>
+//         <li>
+//           <p>Please Please Me</p>
+//           <img src="https://ia801905.us.archive.org/20/items/mbid-ee397a6d-c56f-45ea-9955-d203d2fea708/mbid-ee397a6d-c56f-45ea-9955-d203d2fea708-15141015861.jpg" />
+//         </li>
+//         <li>
+//           <p>Please Please Me</p>
+//           <img src="https://ia801905.us.archive.org/20/items/mbid-ee397a6d-c56f-45ea-9955-d203d2fea708/mbid-ee397a6d-c56f-45ea-9955-d203d2fea708-15141015861.jpg" />
+//         </li>
+//       </ul>
+//     </section>
+//   )
+// }
