b86ad92d507adc14b98c4a52aa33aa116f284bd6
Brittany Houtz <brittany.houtz@gmail.com>
2020-02-06 16:30:52 -0800

    hooks update

--- src/components/ArtistDetail.js
+++ src/components/ArtistDetail.js
@@ -1,43 +1,24 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-// import {  } from hooks
+import { Link } from 'react-router-dom';
+// import { useArtist } from hooks
 
-const ArtistDetail = ({ match }) => {
-  const artist = useArtist(match.params.id);
+const ArtistDetail = () => {
+  const releases = useArtist();
 
-  const releaseLinks = artist.release.map(url => (
-    <a key={url} href={url}>Release {url.split('/').splice(-1)[0]}</a>
-  ));
-
-  const 
+  const artistReleases = releases.map(release => (
+    <li key={artist.name}>
+      <Link to={`/artist/${artist.name}`}>
+      <img src={artwork} />
+      <p>{release.title}</p>
+      </Link>
+    </li>
+  ))
 
   return (
-    <section>
-      <h1>The Beatles</h1>
       <ul>
-        <li>
-          <p>{release.name}</p>
-          <img src="https://ia801905.us.archive.org/20/items/mbid-ee397a6d-c56f-45ea-9955-d203d2fea708/mbid-ee397a6d-c56f-45ea-9955-d203d2fea708-15141015861.jpg" />
-        </li>
-        <li>
-          <p>{release.name}</p>
-          <img src="https://ia801905.us.archive.org/20/items/mbid-ee397a6d-c56f-45ea-9955-d203d2fea708/mbid-ee397a6d-c56f-45ea-9955-d203d2fea708-15141015861.jpg" />
-        </li>
-        <li>
-          <p>{release.name}</p>
-          <img src="https://ia801905.us.archive.org/20/items/mbid-ee397a6d-c56f-45ea-9955-d203d2fea708/mbid-ee397a6d-c56f-45ea-9955-d203d2fea708-15141015861.jpg" />
-        </li>
+        {artistReleases}
       </ul>
-    </section>
   );
 };
 
-ReleaseDetail.propTypes = {
-  match: PropTypes.shape({
-    params: PropTypes.shape({
-      id: PropTypes.string
-    })
-  })
-};
-
-export default CharacterDetail;
+export default ArtistDetail;
